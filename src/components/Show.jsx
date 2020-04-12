9bb2d1e3c080e997f1ba1f3aafad37314aef30ef
TomasLlamas <35276771+TomasLlamas@users.noreply.github.com>
2020-04-12 12:13:07 -0400

    BLEH

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -23,6 +23,7 @@ const ShowArea = styled.div`
 const TitleNav = styled.div`
   display: flex;
   flex-direction: row;
+
 `;
 
 class Show extends Component {
@@ -164,10 +165,11 @@ class Show extends Component {
     return (
       <Container>
         <TitleNav className="mini-cont">
-          <TitleView>View</TitleView>
+          <TitleView className="view-cont">View</TitleView>
           <div className="download-button-cont">
           <button className="download-button" onClick={this.downloadFile}><img className="download-button" src="https://lh3.googleusercontent.com/HNIvuL4rRSjxldDWQ2zUv-FAqftrRTs-_CguHK4AIpmentVtCGtSVywH0J8y8W5L-lOBZ0fUT0QJU0d17pda6Y8F31Y_BkmgT2MUe1djpoxlhzILsvmIYrVs1tjHm7wW5yD78cQp6r2aHumUrIhbeHeOJKs6rMIizH3ouHQrzgEm8zxiq1NV3TAmkuLH_-Nyc0AAQ9Xsvi09iv8jHeqGPauCCOMaH7weRQ2yykMpUkEnvPllslQsWxTUa5r_X6YE2LsQyVgKlWQWFi4NZsWQAeLJ9cwvb-NVIYgyxfkFcA1Fq4amP4KFd64AVadKs1kvnt1C1B1Lp6dKjrYqw7Nr1yxzWeyweRvI9r1ZXi_3QlAgxCOAQ34PhvJzOvijWtnlyUuISJZnsBSYcLf27uqPZkgTyrwRwOxZQMG4ehVnBErCpKz0p_jPFXIWdJiRGBrv7P3ZsEGI1fAZUd67EH-KBaYAXEvUsqwqe9T0z0W8adQgtjNtaqjz1ItDPfHXNOF-2lZWOPm9KZDYCvAxs5vo5gV02J_p5hdVAaL8I_HVNxMe2o7U1sdnl9EtxQLrMdZA3Qxt49G1edOiPtlulvqft1N69nYg__pDZm5IoVU6PhjiA2brRi86PgbZ2Hg_vn2mTsVbM39eSQ0c_gR8SlODS2syazXQq1IIdiJm-hwyA0f73npBEoyZUAWvh-8FWn-epVxGXR4vFcLXah_FL4SnEkBBiNcdeMDJcALfO70RsyUmo9gu-VavIQ=s512-no"></img></button>
           </div>
+          <div id="styling">BLEHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH</div>
         </TitleNav>
         <ShowArea className="show-area">
           <table>
