8256553124837ec2a3e4aeabc1530a215cfeab2a
nmtan <nmtan@umich.edu>
2020-04-12 10:31:00 -0400

    sadf

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -32,7 +32,6 @@ class Parent extends Component {
     return (
       <div>
         <Header />
-<<<<<<< HEAD
 
         <FlexRow>
           <SplitPane split="vertical" defaultSize={700}>
@@ -44,20 +43,6 @@ class Parent extends Component {
             </div>
           </SplitPane>
         </FlexRow>
-=======
-        {/* <EditCont> */}
-          <FlexRow>
-            <SplitPane split="vertical" defaultSize={700} minSize={400}>
-              <div>
-                <Editor value={this.state.hdText} onChange={this.onHDChange} />
-              </div>
-              <div>
-                <Show value={this.state.hdText} />
-              </div>
-            </SplitPane>
-          </FlexRow>
-        {/* </EditCont> */}
->>>>>>> 1960b3e27f0ec52d00da1a43e306c2c7844881b5
       </div>
     );
   }
