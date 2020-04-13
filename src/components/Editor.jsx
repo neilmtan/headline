a2b5d1fadff4732417e08a36135a1fdc1b109db2
nmtan <nmtan@umich.edu>
2020-04-13 19:24:17 -0400

    asdf

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -58,13 +58,13 @@ class Editor extends Component {
     return (
       <Container>
         <TitleView className="mini-title">Editor</TitleView>
-        {/* <EditorArea
+        <EditorArea
             placeholder="Start typing your Headline here!"
             value={this.props.value}
             onChange={this.handleChange}
-          /> */}
+          />
 
-        <MentionsInput
+        {/* <MentionsInput
           className="EditorArea"
           value={this.props.value}
           onChange={this.handleChange}
@@ -75,13 +75,13 @@ class Editor extends Component {
             trigger="#"
             data={BlockTypes}
             // renderSuggestion={this.renderUserSuggestion}
-          />
+          /> */}
           {/* <Mention
             trigger="["
             data={PropertyTypes}
             // renderSuggestion={this.renderTagSuggestion}
           /> */}
-        </MentionsInput>
+        {/* </MentionsInput> */}
       </Container>
     );
   }
